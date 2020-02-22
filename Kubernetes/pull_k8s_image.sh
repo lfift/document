ae75343aad0d31d51cbe36e7d449f2eda23a1c7a
ift520 <liu.ift@gmail.com>
2020-02-22 16:58:44 +0800

    整理文件目录，增加K8S、Docker相关文档

---
+++ Kubernetes/pull_k8s_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+url=registry.cn-shanghai.aliyuncs.com/ift520
+version=v1.17.3
+images=(`kubeadm config images list --kubernetes-version=$version|awk -F '/' '{print $2}'`)
+for imagename in ${images[@]} ; do
+  docker pull $url/$imagename
+  docker tag $url/$imagename k8s.gcr.io/$imagename
+  docker rmi -f $url/$imagename
+done
